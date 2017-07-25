7b1d80c6f607cc30fb541be71a38a60962a34e6f
Woodsmike <Woodsmike@users.noreply.github.com>
2017-07-24 21:06:10 -0400

    initial commit

---
+++ CDExchangeMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CDExchangeMVC.Startup))]
+namespace CDExchangeMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
